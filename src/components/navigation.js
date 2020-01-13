e6f8729223641e927b2c204fe5726210ad932708
Hector Salazar <hector.sa@gmail.com>
2020-01-13 17:41:32 +1100

    Adding navigation items

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -9,6 +9,15 @@ export default () => (
         <Link to="/">Home</Link>
       </li>
       <li className={styles.navigationItem}>
+        <Link to="/investing/">Investing</Link>
+      </li>
+      <li className={styles.navigationItem}>
+        <Link to="/passive-income/">Passive Income</Link>
+      </li>
+      <li className={styles.navigationItem}>
+        <Link to="/decentralized-finance/">Decentralized Finance</Link>
+      </li>
+      <li className={styles.navigationItem}>
         <Link to="/blog/">Blog</Link>
       </li>
     </ul>
